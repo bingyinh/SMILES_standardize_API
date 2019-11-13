bf28e5f640d6142eb3e4ebaf0a299ab6b7602007
bingyinh <bingyin.hu@gmail.com>
2019-11-13 15:03:47 -0500

    updated for python3, using mechanicalsoup instead of mechanize

--- SMILEStrans.py
+++ SMILEStrans.py
@@ -1,51 +1,43 @@
-import mechanize
+import mechanicalsoup
 from bs4 import BeautifulSoup
 
 class SMILEStrans(object):
     def __init__(self, smiles = ''):
-        self.br = mechanize.Browser()
-        self.br.set_handle_robots(False)
-        self.br.set_handle_redirect(False)
-        self.br.set_handle_refresh(False)
-        self.br.set_debug_redirects(True)
-        self.br.addheaders=[('User-Agent', 'Mozilla/5.0'),
-                            ('Cache-Control', 'max-age=0'),
-                            ('Connection', 'keep-alive')]
+        self.br = mechanicalsoup.StatefulBrowser(user_agent='Mozilla/5.0')
         self.br.open('https://cactus.nci.nih.gov/translate/index.html#Form')
         self.SMILES = smiles
         self.brtxt = None
         self.trans = ''
-        self.response = None
-        self.responsetxt = ''
+        self.resp = None
         self.result = ''
 
     def setSMILES(self, smiles):
-        if isinstance(smiles, str) or isinstance(smiles, unicode):
+        if isinstance(smiles, str):
             self.__init__()
             self.SMILES = smiles
             print("SMILES: %s" %(self.SMILES))
             self.trans = ''
 
     def translate(self):
-        self.br.select_form(name="form")
-        self.brtxt = self.br.form.find_control("smiles")
-        if self.brtxt is None:
-            raise ValueError("[Form] Cannot translate %s" %(self.SMILES))
-        self.brtxt.value = self.SMILES
-        self.response = self.br.submit()
-        self.responsetxt = self.response.read()
-        print 
-        soup = BeautifulSoup(self.responsetxt, 'html.parser')
-        if soup.title.text == u'USMILES Result':
+        form = self.br.select_form("form")
+        form.set("smiles", self.SMILES)
+        self.resp = self.br.submit_selected()
+        soup = BeautifulSoup(self.resp.text, 'html.parser')
+        if soup.title.text == 'USMILES Result':
             if soup.find('b') is not None:
                 self.result = soup.find('b').text
+                self.br.close()
                 return self.result
             else:
+                self.br.close()
                 raise ValueError("[Parse] Cannot find results in the page returned.")
-        elif soup.title.text == u'Translation Error':
+        elif soup.title.text == 'Translation Error':
             if soup.find('h3') is not None:
+                self.br.close()
                 raise ValueError(soup.find('h3').text)
             else:
+                self.br.close()
                 raise ValueError('[Translation] Unknown translation error.')
         else:
+            self.br.close()
             raise ValueError('Unknown error.')
